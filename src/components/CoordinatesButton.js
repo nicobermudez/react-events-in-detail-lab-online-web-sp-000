e24ad582c46b8809e8486d0b026a605f588154d2
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 19:36:36 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 export default class CoordinatesButton extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      
-    }
-  }
+
 
   render() {
     return(
