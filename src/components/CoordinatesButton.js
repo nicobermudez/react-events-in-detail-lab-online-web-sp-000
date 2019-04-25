abd33054cf15f5e1d5ddfaaad01b447c7c54f18e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 19:38:42 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,7 +4,8 @@ export default class CoordinatesButton extends Component {
 
   findCoordinates = (event) => {
     const coordinatesArray = [
-      
+      event.clientX,
+      event.clientY
     ]
   }
 
